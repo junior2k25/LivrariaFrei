101e864de9147e0438daf770abc830a86b72ccfe
Sergio <ra44698525896@acaonsfatima.org.br>
2025-10-06 10:23:37 -0300

    alterado

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -8,7 +8,9 @@ export default function Inicio() {
         <div className="container-inicio">
         <Cabecalho />
         <hr />
+        <main>
             <p>seja bem-vindo à nossa livraria! Aqui, a paixão pela leitura ganha vida. Nossa missão é espalhar o amor pelos livros e criar um espaço acolhedor para todos os amantes da literatura. Conheça nossa equipe dedicada e fique por dentro dos eventos emocionantes que promovemos. Venha explorar um mundo de histórias e conhecimento!</p>
+            </main>
             <Rodape />
              </div>
     )
